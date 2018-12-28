ba7887e3eb815098535ee2d3b330ba6a245a0e7a
Shivam Gandotra <shivam.prof22@gmail.com>
2018-12-28 21:15:58 +0530

    Hello World App

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello World App")),
+        body: Center(child: Text("Hello World"))
+      ),
+    );
+  }
+}
